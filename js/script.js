a055f051d75d064815d21da76843416195c3bb91
DESKTOP0001 <DESKTOP0001@DESKTOP-PTBIQT9>
2017-06-04 20:57:46 -0500

    sdfsd

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlidesx");
+  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
