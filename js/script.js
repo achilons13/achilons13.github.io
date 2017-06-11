c9e9f49803aed82e29482439a804717717c3d067
DESKTOP0001 <DESKTOP0001@DESKTOP-PTBIQT9>
2017-06-11 18:04:34 -0500

    ggff

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
  * 
  */
 // Automatic Slideshow - change image every 4 seconds
-		
-		/*ACTIVAR SLIDER DE ACTIVIDADES*/
-		 
-		  $(function() {
-		    $(".rslides").responsiveSlides();
-		  });
-		
-		/*FIN DE SLIDER*/
+/* ACTIVAR SLIDER DE ACTIVIDADES */
+$(window).load(function() {
+	$(function() {
+		$(".rslides").responsiveSlides();
+	});
+
+});
+/* FIN DE SLIDER */
+
